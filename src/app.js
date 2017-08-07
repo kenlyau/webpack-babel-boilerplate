8718678cd167d0e0038cb129df47dfc755ee880c
kenlyau <newallah@gmail.com>
2017-08-07 21:56:33 +0800

    edit index

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 export default {
   run(){
+    let str = 'start app!';
+    let h1 = document.createElement('h1');
+    h1.className = 'text-center';
+    h1.innerText = str;
+    document.body.appendChild(h1);
     console.log('success!**');
     return true;
   }
